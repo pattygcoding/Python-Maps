95623001c0631a95f9c09f779b7433fbe303df41
Patrick Goodwin <pattygsocials@gmail.com>
2024-09-19 15:00:35 -0400

    Create state_county_map.py

---
+++ state_county_map.py
@@ -0,0 +1,28 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Load a shapefile of US counties
+url = 'https://www2.census.gov/geo/tiger/GENZ2021/shp/cb_2021_us_county_500k.zip'
+gdf = gpd.read_file(url)
+
+# Filter for South Carolina counties (FIPS code for South Carolina is 45)
+sc_counties = gdf[gdf['STATEFP'] == '45']
+
+# List of upstate counties to shade in red
+upstate_counties = [
+    'Abbeville', 'Anderson', 'Cherokee', 'Greenville', 
+    'Greenwood', 'Laurens', 'Oconee', 'Pickens', 
+    'Spartanburg', 'Union'
+]
+
+# Mark those counties to shade in red
+sc_counties['color'] = sc_counties['NAME'].apply(lambda x: 'red' if x in upstate_counties else 'lightgray')
+
+# Plot South Carolina counties with the upstate ones shaded red
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+sc_counties.plot(ax=ax, color=sc_counties['color'], edgecolor='black')
+
+# Add a title and show the plot
+plt.title("Upstate South Carolina Counties - Selected Counties Shaded Red", fontsize=15)
+plt.axis('off')  # Hide axes
+plt.show()
